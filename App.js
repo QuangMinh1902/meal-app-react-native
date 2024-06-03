1ece9fef9fc9ed89af877f7007fc0a6a00388462
Quang Minh PHAM <quangminh1575@gmail.com>
2024-06-03 19:45:41 +0200

    fea: update

--- App.js
+++ App.js
@@ -3,9 +3,12 @@ import { StyleSheet, Text, View } from "react-native";
 import CategoriesScreen from "./screens/CategoriesScreen";
 
 export default function App() {
-  return <CategoriesScreen />;
+  return (
+    <>
+      <StatusBar style="light" />
+      <CategoriesScreen />
+    </>
+  );
 }
 
-const styles = StyleSheet.create({
-  container: {},
-});
+const styles = StyleSheet.create({});
--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,13 +1,52 @@
-import { Pressable, View, Text } from "react-native";
+import { Pressable, View, Text, StyleSheet, Platform } from "react-native";
 
 export default function CategoryGridTile({ title, color }) {
   return (
-    <View>
-      <Pressable>
-        <View>
-          <Text>{title}</Text>
+    <View style={styles.gridItem}>
+      <Pressable
+        style={({ pressed }) => [
+          styles.button,
+          pressed ? styles.buttonPressed : null,
+        ]}
+      >
+        <View style={[styles.innerContainer, { backgroundColor: color }]}>
+          <Text style={styles.title}>{title}</Text>
         </View>
       </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  gridItem: {
+    flex: 1,
+    margin: 16,
+    height: 150,
+    shadowColor: "black",
+    shadowOpacity: 0.25,
+    shadowOffset: { width: 2, height: 2 },
+    shadowRadius: 8,
+    overflow: Platform.OS === "android" ? "hidden" : "visible", // sure that the ripple doesn't go beyond the corner
+  },
+
+  buttonPressed: {
+    opacity: 0.5,
+  },
+
+  button: {
+    flex: 1,
+  },
+
+  innerContainer: {
+    borderRadius: 8,
+    flex: 1,
+    padding: 16,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+
+  title: {
+    fontWeight: "bold",
+    fontSize: 18,
+  },
+});
