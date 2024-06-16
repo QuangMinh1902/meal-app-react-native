8bfc37284cf9a7565c8021946ae777159f6ce713
Quang Minh PHAM <quangminh1575@gmail.com>
2024-06-16 21:46:56 +0200

    update

--- components/IconButton.js
+++ components/IconButton.js
@@ -5,7 +5,7 @@ export default function IconButton({ icon, color, onPress }) {
   return (
     <Pressable
       onPress={onPress}
-      style={({ pressed }) => (pressed ? styles.pressed : null)}
+      style={({ pressed }) => pressed && styles.pressed}
     >
       <Ionicons name={icon} size={24} color={color} />
     </Pressable>
